df86d6910d75b03cbe66af5962c8da33dfbdfa33
Andre Roque <mateusandre1@yahoo.com>
2019-11-07 19:53:45 -0500

    home page is done

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,20 +5,18 @@ class Footer extends Component {
   render() {
     return (
       <React.Fragment>
-        <footer className="footer">
-          <div className="container text-center mx-auto">
+        <footer className="footer text-center mx-auto text-white">
             <i className="far fa-copyright"></i> 2019 Andre Roque
-            <a href="https://github.com/MateusAndre1">
+            <a id="facebook2" href="https://github.com/MateusAndre1">
               <span className="ml-3" id="facebook2">
                 <i className="fab fa-github-square"></i>
               </span>
             </a>
-            <a href="https://www.linkedin.com/in/andremroque/">
+            <a id="linkedin2" href="https://www.linkedin.com/in/andremroque/">
               <span id="linkedin2">
                 <i className="fab fa-linkedin ml-3"></i>
               </span>
             </a>
-          </div>
         </footer>
       </React.Fragment>
     );
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,10 +6,27 @@ class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                    <VideoBg />
+                <VideoBg />
+                <div className="row">
+                    <div className="col p-0">
+                        <div className="leftSide text-right">
+                            <p className="leftName">ANDRE</p><p>Where reaching new</p>
+                        </div>
+                    </div>
+                    <div className="col p-0">
+                        <div className="rightSide">
+                            <p className="rightName">ROQUE</p><p className="rightP">heights is possible</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="container text-center">
+                <a class="btn btn-black mt-5 btn1" href="#portfolio-section">
+                    Explore My Work
+                </a>
+                </div>
             </React.Fragment>
-                    )
-                }
-            }
-            
+        )
+    }
+}
+
 export default Home
